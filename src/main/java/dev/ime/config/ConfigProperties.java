0828213167bb9d254ef40e859da1314c8cd70fe1
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-08-25 08:41:02 +0200

    feat: add config properties

---
+++ src/main/java/dev/ime/config/ConfigProperties.java
@@ -0,0 +1,20 @@
+package dev.ime.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+public class ConfigProperties {
+
+	private String key;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}	
+	
+}
